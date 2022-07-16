c3cd0c3c656aed5da490c375384fa54b4d87fd44
IreneuszGorak <109158627+IreneuszGorak@users.noreply.github.com>
2022-07-16 09:38:41 +0200

    added autoprefixer and sourucemap to dev dep.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 const rename = require('gulp-rename');
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
 
 gulp.task('sass', () => {
   return gulp
     .src('./sass/main.scss')
     .pipe(rename('style.css'))
+    .pipe(sourcemaps.init())
     .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./css/'));
 });
 
