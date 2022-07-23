00fd508b5dbf2c15eb551197565d34b8f2630297
IreneuszGorak <109158627+IreneuszGorak@users.noreply.github.com>
2022-07-23 13:51:36 +0200

    nav menu fix, media q. fix

--- js/index.js
+++ js/index.js
@@ -6,6 +6,14 @@ hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
 
+const ctaSelected = document.querySelectorAll('.header-main-nav-link_nav-cta');
+ctaSelected.forEach((n) =>
+  n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  })
+);
+
 const linkSelected = document.querySelectorAll('.header-main-nav-link');
 linkSelected.forEach((n) =>
   n.addEventListener('click', () => {
@@ -35,6 +43,12 @@ allLinks.forEach((link) =>
     if (href !== '#' && href.startsWith('#')) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    //close mobile nav
+    if (link.classList.contains('active')) {
+      hamburger.classList.remove('active');
+      navMenu.classList.remove('active');
     }
   })
 );
